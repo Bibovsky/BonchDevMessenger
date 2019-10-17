e3bb82f86d49306799e1d168a1e01cb5df68e898
Bibovsky <48915487+Bibovsky@users.noreply.github.com>
2019-10-17 20:17:12 +0300

    Start

---
+++ app/src/main/java/bonch/dev/school/ui/fragments/ProfileFragment.kt
@@ -0,0 +1,20 @@
+package bonch.dev.school.ui.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import bonch.dev.school.R
+
+class ProfileFragment:Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.profile_fragment,container,false)
+        return view
+    }
+}
